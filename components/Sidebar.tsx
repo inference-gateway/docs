1abb0e4439cbe8bafab0a35efc0f4f7d5b09cb83
Eden Reich <eden.reich@gmail.com>
2026-01-23 21:51:30 +0100

    refactor(docs): Remove UI related documentation
    The new replacement is the CLI web interface.
    
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -50,7 +50,6 @@ const sections = [
       { title: 'Examples', href: '/examples' },
       { title: 'IDEs', href: '/ides' },
       { title: 'Observability', href: '/observability' },
-      { title: 'UI Components', href: '/ui' },
     ],
   },
 ];
